29937622cef4c7dda8e1d5a816bd270cd6cb2e30
kimyongchul <dna119@naver.com>
2020-06-22 23:42:56 +0900

    commit swift1 project

---
+++ swift1/swift1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift1
+//
+//  Created by kim yongchul on 2020/06/22.
+//  Copyright © 2020 kim yongchul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
